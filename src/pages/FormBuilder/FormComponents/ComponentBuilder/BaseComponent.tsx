5ecc6164a2e1db466215f98a4cd1034745ddb221
Ujjawal Shrivastava <ujjawalshrivastava19@gmail.com>
2020-07-28 13:17:56 +0530

    Fields are now RESPONSIVE

--- src/pages/FormBuilder/FormComponents/ComponentBuilder/BaseComponent.tsx
+++ src/pages/FormBuilder/FormComponents/ComponentBuilder/BaseComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BaseComponent(props: any) {
     return (
         <div className="box" style={{ padding: "0rem", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", overflow: "hidden", margin: "1rem 0rem" }} >
-            <div style={{display:"flex", flexDirection:"row", alignItems:'center',justifyContent:"space-between", width:"100%"}}>
+            <div className="component-data">
                 <p className="px-4 py-4" style={{wordBreak:"break-word"}}>{props.component.title.toUpperCase()}</p>
                 <div style={{ padding: "0.8rem" }}>
                     <span className="tag is-link has-text-weight-bold is-success is-light is-rounded">{props.component.type.toUpperCase()}</span>
