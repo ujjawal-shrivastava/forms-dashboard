1cffd45b639dc9a62b9dcc3e61fa1b71e2a3793e
Ujjawal Shrivastava <ujjawalshrivastava19@gmail.com>
2020-09-18 15:58:40 +0530

    Added data for dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,12 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import './Dashboard.scss'
 import forms from '../../assets/forms.svg';
 import Card from './Components/Card';
 import Actions from './Components/Actions';
 import YourForms from './Components/YourForms';
 import { Redirect } from 'react-router-dom';
-import {UserContext} from '../../UserContext'
-import {useQuery } from 'react-apollo-hooks';
+import { UserContext } from '../../UserContext'
+import { useQuery } from 'react-apollo-hooks';
 import { gql } from 'apollo-boost'
 
 const DATA = gql`
@@ -22,15 +22,16 @@ const DATA = gql`
 
 export default function Dashboard() {
     const [user, setUser] = useContext(UserContext)
-    const { data, loading, error } = useQuery(DATA, { errorPolicy: 'all', fetchPolicy:'cache-and-network', pollInterval:21000 });
+    const { data, loading, error } = useQuery(DATA, { errorPolicy: 'all', fetchPolicy: 'cache-and-network', pollInterval: 21000 });
     document.title = "Dashboard - DeForm";
-    if(!user.auth) return(<Redirect to="/login" />)
+    if (!user.auth) return (<Redirect to="/login" />)
+    if (error) return (<h1>{error.message}</h1>)
     return (
         <section className="section">
             <div className="columns mb-5">
-                <Card title="Forms" value={loading?"-":data.userData.forms} color="#eb3b5a" img={forms}/>
-                <Card title="Responses" value={loading?"-":data.userData.responses} color="#ff9f43" img={forms}/>
-                <Card title="Visits" value={loading?"-":data.userData.views} color="#34495e" img={forms}/>
+                <Card title="Forms" value={loading ? "-" : data.userData.forms} color="#eb3b5a" img={forms} />
+                <Card title="Responses" value={loading ? "-" : data.userData.responses} color="#ff9f43" img={forms} />
+                <Card title="Visits" value={loading ? "-" : data.userData.views} color="#34495e" img={forms} />
             </div>
             <p className="is-size-4 has-text-weight-bold mb-3 ml-3" style={{ color: '#3d3d3d' }}>Recent</p>
             <div className="columns">
